1082722ddd3ba5d06dce2cc036d2c3ebca521f7c
Aden-Ibrahim <aden.aio@hotmail.com>
2024-09-24 15:36:36 +0200

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -6,45 +6,42 @@ dictionary = {
 
 """Hur skulle du identifiera vilket projekt Erik har spenderat mest tid på?"""
 
-def projekt_med_flest_timmar(projekt, namn): # Denna funktion berättar vilket projekt som en person har spenderat mest tid på
-    
+def projekt_med_flest_timmar(projekt_lista, anställd): # Denna funktion berättar vilket projekt som en person har spenderat mest tid på
+
     timmar = [0]
-    for i in projekt:
-        for j in projekt[i]:
-            if namn in j:
-                timmar.append(projekt[i][j])
-    else:
-        if timmar == [0]:
-            print("Personen finns ej med i några projekt")
-        else:    
-            for i in projekt:
-                for j in projekt[i]:
-                    if namn in j and projekt[i][j] == max(timmar):
-                        return print(i)
+    
+    for projekt in projekt_lista:
+        for projekt_anställd in projekt_lista[projekt]:
+            if anställd == projekt_anställd:
+                timmar.append(projekt_lista[projekt][projekt_anställd])
+
+    if timmar == [0]:
+        print("Personen finns ej med i några projekt")
+
+    else:    
+        for projekt in projekt_lista:
+            for projekt_anställd in projekt_lista[projekt]:
+                if anställd == projekt_anställd and projekt_lista[projekt][projekt_anställd] == max(timmar):
+                    return print(projekt)
     
 projekt_med_flest_timmar(dictionary, "Erik")
 
 
-
 """Hur skulle du skapa en ny dictionary som bara innehåller projekten där Lina har arbetat mer än 30 timmar?"""
 
-def uppdaterad_dictionary(projekt, namn): # Denna funktion skapar en ny dictionary beroende på om personen har arbetat mer en 30 timmar i något projekt
+def uppdaterad_dictionary(projekt_lista, anställd): # Denna funktion skapar en ny dictionary beroende på om personen har arbetat mer en 30 timmar i något projekt
     
     ny_projekt = {}
-    for i in projekt:
-            for j in projekt[i]:
-                if namn in j and projekt[i][j] > 30:
-                    ny_projekt.update({i: projekt[i]})
+
+    for projekt in projekt_lista:
+        for projekt_anställd in projekt_lista[projekt]:
+            if anställd == projekt_anställd and projekt_lista[projekt][projekt_anställd] > 30:
+                ny_projekt.update({projekt: projekt_lista[projekt]})
+    
+    if ny_projekt == {}:
+        print("Personen finns ej med i några projekt i mer än 30 timmar")
+
     else:
-        if ny_projekt == {}:
-           print("Personen finns ej med i några projekt")
-        else:
-            return print(ny_projekt)
+        return print(ny_projekt)
 
 uppdaterad_dictionary(dictionary, "Lina")
-                
-
-
-
-
-
